c2336f1c060cdb3c800dcf0973bd0f7923dd4108
queenxy <1060382399@qq.com>
2023-11-25 12:53:18 +0800

    add joint space control

--- src/tinker_moveit/src/arm_controller.cpp
+++ src/tinker_moveit/src/arm_controller.cpp
@@ -8,6 +8,8 @@
 #include <moveit_msgs/msg/attached_collision_object.hpp>
 #include <moveit_msgs/msg/collision_object.hpp>
 
+#include <std_msgs/msg/bool.hpp>
+#include <std_srvs/srv/trigger.hpp>
 #include <tinker_msgs/srv/ur_control_service.hpp>
 
 #include <memory>
@@ -21,8 +23,10 @@ class ArmController : public rclcpp::Node
       Node("tinker_arm_controller"),
       move_group(nullptr)    //move group class need the shared_ptr of current node, so we initialize it later.
     {
-        service = this->create_service<tinker_msgs::srv::URControlService>("tinker_arm_control_service", 
+        control_service = this->create_service<tinker_msgs::srv::URControlService>("tinker_arm_control_service", 
                             std::bind(&ArmController::callback, this, std::placeholders::_1, std::placeholders::_2));
+        initial_service = this->create_service<std_srvs::srv::Trigger>("tinker_arm_initial_service", 
+                            std::bind(&ArmController::initial, this, std::placeholders::_1, std::placeholders::_2));
     }
 
     bool init_move_group(std::shared_ptr<moveit::planning_interface::MoveGroupInterface> _move_group) {
@@ -30,14 +34,16 @@ class ArmController : public rclcpp::Node
     }
 
   private:
-    rclcpp::Service<tinker_msgs::srv::URControlService>::SharedPtr service;
+    rclcpp::Service<tinker_msgs::srv::URControlService>::SharedPtr control_service;
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr initial_service;
 
     std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group;    // we pass the shared_ptr of move group
     moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    const moveit::core::JointModelGroup* joint_model_group;
 
     void callback(const std::shared_ptr<tinker_msgs::srv::URControlService::Request> request,
           std::shared_ptr<tinker_msgs::srv::URControlService::Response> response);
+    void initial(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+          std::shared_ptr<std_srvs::srv::Trigger::Response> response);
 
 };
 
@@ -45,6 +51,41 @@ void ArmController::callback(const std::shared_ptr<tinker_msgs::srv::URControlSe
           std::shared_ptr<tinker_msgs::srv::URControlService::Response> response)
 {   
     move_group->setPoseTarget(request->target_pose);
+
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+    bool plan_success = (move_group->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+    bool move_success = (move_group->move() == moveit::core::MoveItErrorCode::SUCCESS);
+
+    RCLCPP_INFO(this->get_logger(), "Planning (pose goal) %s", plan_success ? "" : "FAILED");
+    RCLCPP_INFO(this->get_logger(), "Move to (pose goal) %s", move_success ? "" : "FAILED");
+
+    response->success = move_success;
+
+}
+
+void ArmController::initial(const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+          std::shared_ptr<std_srvs::srv::Trigger::Response> response)
+{   
+    const moveit::core::JointModelGroup* joint_model_group;
+    moveit::core::RobotStatePtr current_state = move_group->getCurrentState(10);
+    std::vector<double> joint_group_positions;
+    current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
+
+    joint_group_positions[0] = -1.0;  // radians
+    joint_group_positions[1] = -1.0;
+    joint_group_positions[2] = -1.0;
+    joint_group_positions[3] = -1.0;
+    joint_group_positions[4] = -1.0;
+    joint_group_positions[5] = -1.0;
+    bool within_bounds = move_group->setJointValueTarget(joint_group_positions);
+    if (!within_bounds)
+    {
+      RCLCPP_WARN(this->get_logger(), "Target joint position(s) were outside of limits, but we will plan and clamp to the limits ");
+    }
+
+    move_group->setMaxVelocityScalingFactor(0.05);
+    move_group->setMaxAccelerationScalingFactor(0.05);
 
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
